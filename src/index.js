c5365dea8a5bd23e455360ea93ab6dcc09b3810f
ismailAbdirahman <ismail960i960@gmail.com>
2022-12-31 20:58:12 +0300

    fixed bug

--- src/index.js
+++ src/index.js
@@ -46,8 +46,8 @@ cloudinary.config({
 //----------------------------------------------------------------------------------//
 const app = express()
 
-//const PORT = process.env.PORT ||4000
-const PORT = 4000
+const PORT = process.env.PORT ||4000
+//const PORT = 4000
 
 
 app.use(express.json())
