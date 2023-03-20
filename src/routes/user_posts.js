d8d12053979c1fa28900079929612d0505f728ec
ismailAbdirahman <ismail960i960@gmail.com>
2023-03-20 09:57:32 +0300

    fixed like and unlike error

--- src/routes/user_posts.js
+++ src/routes/user_posts.js
@@ -86,19 +86,20 @@ router.get('/userpost/likedPost/:id', auth, async (req, res) => {
 
             post.likes.pull(userID)
             myProfile.likedPosts.pull(postID)
+            await myProfile.save()
             const result = await post.save()
             const likedUsersList = await result.likes
 
-            return res.send({likedUsersList})
+            return res.send({ likedUsersList })
         }
 
         post.likes = post.likes.concat(userID)
         myProfile.likedPosts = myProfile.likedPosts.concat(postID)
-        await post.save()
+        await myProfile.save()
         const result = await post.save()
-            const likedUsersList = await result.likes
+        const likedUsersList = await result.likes
 
-            return res.send({likedUsersList})
+        return res.send({ likedUsersList })
 
 
     } catch (error) {
